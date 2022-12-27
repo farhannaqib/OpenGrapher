4a11fcffb18084556373016fa0aacfc0699ecbbb
Farhan Naqib <fn72@cornell.edu>
2022-12-26 21:26:06 -0500

    negatives

--- shuntingyard.cpp
+++ shuntingyard.cpp
@@ -5,26 +5,20 @@
 
 #include "tokenizer.cpp"
 
-void testTokenizer() {
-    std::string input("1 + (2-3)/4");
+int main() {
+    std::string input("-1+(22-3)/4");
     std::queue<Token> q;
 
-    while (!input.empty()) q.push(read(input));
+    while (!input.empty()) {
+        if (!q.empty() && (q.back().type == TokenType::RB 
+        || q.back().type == TokenType::NUM)) 
+        q.push(read(input, true));
+        else q.push(read(input, false));
+    }
     
     while (!q.empty()) {
         std::cout << q.front().data << std::endl;
         q.pop();
-    }
-}
-
-int main() {
-    std::string input("1 + (2-3)/4");
-    std::queue<Token> output;
-    std::stack<Token> stack;
-
-    while (!input.empty()) {
-        Token token = read(input);
-        if (token.type == TokenType::NUM) output.push(token);
     }
 
     return 0;
--- tokenizer.cpp
+++ tokenizer.cpp
@@ -17,8 +17,8 @@ enum class TokenType {
 // Token representations in string form
 // NUM and ERROR have placeholder values
 static const std::string tokens[] = {
-    "+", "-", "*", "/", "^", "%", 
-    "(", ")", "n", "e"
+    "+", "-", "*", "/", "^", "%", "(", ")",
+    "n", "e"
 };
 
 // Token class that describes a single token
@@ -51,55 +51,41 @@ class NumToken : public Token {
 // while removing its characters. If no match
 // is found, the error token is returned.
 // Whitespace is removed when finding tokens
-Token read(std::string& input) {
+Token read(std::string& input, bool lastWasRBOrNum) {
     Token token;
+    token.setType(TokenType::ERROR);
 
-    // TODO fix the switch
-    switch(input.front()) {
-        case '+':
-            token.setType(TokenType::ADD);
-            break;
-        case '-':
-            token.setType(TokenType::SUB);
-            break;
-        case '*':
-            token.setType(TokenType::MUL);
-            break;
-        case '/':
-            token.setType(TokenType::DIV);
-            break;
-        case '^':
-            token.setType(TokenType::EXP);
-            break;
-        case '%':
-            token.setType(TokenType::MOD);
-            break;
-        case '(':
-            token.setType(TokenType::LB);
-            break;
-        case ')':
-            token.setType(TokenType::RB);
-            break;
-        default:
-            token.setType(TokenType::ERROR);
-            break;
-    }
-    if (token.type != TokenType::ERROR) {
-        input.erase(0, 1);
-        return token;
-    }
-    else {
-        std::string t;
-        while (isdigit(input.front())) {
-            t.append(1, input.front());
+    while(isspace(input.front())) input.erase(0, 1);
+
+    // checks through all 1-char tokens
+    bool negValue = false;
+    for (int i = 0; i < 8; i++) {
+        if (input.front() == tokens[i].front()) {
+
+            // accounts for unary negative op
+            if (i == 1 && !lastWasRBOrNum) {
+                negValue = true;
+                input.erase(0, 1);
+                break;
+            }
+
+            token.setType((TokenType) i);
             input.erase(0, 1);
+            return token;
         }
-        if (t == "") return token;
-
-        NumToken numToken;
-        numToken.setNum(t);
-        return numToken;
     }
+
+    std::string t;
+    while (isdigit(input.front())) {
+        t.append(1, input.front());
+        input.erase(0, 1);
+    }
+    if (t == "") return token;
+
+    if (negValue) t.insert(0, "-");
+    NumToken numToken;
+    numToken.setNum(t);
+    return numToken;
 }
 
 #endif
